23b62bd3432373e09caa954cadf801d1db899702
Alex Agaev <alex34agaev@gmail.com>
2022-12-11 20:13:22 +0300

    request fixes

--- src/store/jobsModule.js
+++ src/store/jobsModule.js
@@ -23,7 +23,7 @@ export const jobsModule = {
 	actions: {
 		async getJobs({ commit }) {
 			try {
-				let response = await axios.get("../data.json");
+				let response = await axios.get("/data.json");
 				commit("setJobsData", response.data);
 
 			} catch (e) {
